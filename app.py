3c4cf0295d8ae2d75471300de804295513021437
DomGreg <gregoriosilva87@gmail.com>
2022-02-20 03:11:52 -0300

    Primeiro commit do site do bardo

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+from flask import Flask, render_template
+
+
+app = Flask (__name__)
+
+#API de msg motivacional/rota index do site 
+@app.route("/")
+def index ():
+    busca = requests.get('https://api.adviceslip.com/advice')
+    resul = busca.json()
+    resultado = resul['slip']['advice']
+    return render_template('index.html', msg = resultado)
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
